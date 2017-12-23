5dc35d871ea4ea489f7e05fb94a2d851f492c18c
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2017-12-23 01:14:26 -0800

    12/23: commit changes
    Added new contract file.
    app:js: modify code to adapt to second contract.
    
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -17,22 +17,37 @@ import { default as contract } from 'truffle-contract'
 
 import voting_artifacts from '../../build/contracts/Voting.json'
 var jag=1;
+var contract_type = 2;
 console.log("jag global var is %d", jag);
 if(jag) {
 	console.log("jag debug");
+//first contract
+if(contract_type == 1) {
 	var abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]');
+
+} else {
+//second contract
+	var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"bytes8"}],"name":"votes","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"party_n","outputs":[{"name":"","type":"bytes8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"party","type":"bytes8"}],"name":"count","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"votes_limit","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"party","type":"bytes8"}],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"party","type":"bytes8"}],"name":"valid","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"party","type":"bytes8[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]');
+
+}
 
 	console.log("jag debug 1");
 	var VotingContract = web3.eth.contract(abi);
 	console.log("jag debug 2");
-
-	var jag_contractInstance = VotingContract.at('0x0F435d2b4f90796B17C539a0f0819FC209ff9e93');
+	if(contract_type == 1) {
+		//first contract address - Rinkeby
+		//var jag_contractInstance = VotingContract.at('0x0F435d2b4f90796B17C539a0f0819FC209ff9e93');
+		var jag_contractInstance = VotingContract.at('0xc26294d170f409faffe3c55164575b1f23025522');
+	} else {
+		//second contract address - Rinkeby
+		var jag_contractInstance = VotingContract.at('0x3A8A90eeB87CFfF63A6dE1f721849fD2f02BBE44');
+	}
 }
 if(!jag) {
 	var Voting = contract(voting_artifacts);
 }
 
-let candidates = {"BJP": "candidate-1", "Cong": "candidate-2", "Others": "candidate-3"};
+let candidates = {"bjp": "candidate-1", "con": "candidate-2", "oth": "candidate-3"};
 
 console.log("jag end of main function");
 
@@ -50,16 +65,30 @@ window.voteForCandidate = function(candidate) {
 		let candidateName = form.elements["test"].value;
 		console.log("jag 1 %s",candidateName);
 		let estimate_gas;
-		jag_contractInstance.voteForCandidate.estimateGas(candidateName,{ from: web3.eth.accounts[0]}, function(error, estimate_gas) {
-			console.log("jag estimated gas is " + estimate_gas);
-			estimate_gas += 100;
-			jag_contractInstance.voteForCandidate(candidateName, {gas: estimate_gas, from: web3.eth.accounts[0]}, function(error, result) {
-				console.log("jag "+ result);
-				$("#msg").html("Vote is registered in the blockchain and the hash is" + result);
-				let div_id = candidates[candidateName];
-				total_votes();
-			 });
-      		});
+		
+		if(contract_type == 1) {
+			jag_contractInstance.voteForCandidate.estimateGas(candidateName,{ from: web3.eth.accounts[0]}, function(error, estimate_gas) {
+				console.log("jag estimated gas is " + estimate_gas);
+				estimate_gas += 100;
+      				jag_contractInstance.voteForCandidate(candidateName, {gas: estimate_gas, from: web3.eth.accounts[0]}, function(error, result) {
+					console.log("jag "+ result);
+          				$("#msg").html("Vote is registered in the blockchain and the hash is" + result);
+        				let div_id = candidates[candidateName];
+					total_votes();
+			 	});
+      			});
+		} else {
+			jag_contractInstance.vote.estimateGas(candidateName,{ from: web3.eth.accounts[0]}, function(error, estimate_gas) {
+				console.log("jag estimated gas is " + estimate_gas);
+				estimate_gas += 100;
+      				jag_contractInstance.vote(candidateName, {gas: estimate_gas, from: web3.eth.accounts[0]}, function(error, result) {
+					console.log("jag "+ result);
+          				$("#msg").html("Vote is registered in the blockchain and the hash is" + result);
+        				let div_id = candidates[candidateName];
+					total_votes();
+			 	});
+      			});
+		}
 	} else {
 		let candidateName = $("#candidate").val();
     		$("#candidate").val("");
@@ -105,11 +134,17 @@ function total_votes() {
 	let candidateNames = Object.keys(candidates);
   	for (var i = 0; i < candidateNames.length; i++) {
     		let name = candidateNames[i];
+     		console.log("jag total_votes  %s", name);
     		if(jag) {
-     			console.log("jag total_votes  %s", name);
-      			jag_contractInstance.totalVotesFor.call(name, function(error, v) {
-        		$("#" + candidates[name]).html(v.toString());
-      		});
+			if(contract_type == 1) {
+      				jag_contractInstance.totalVotesFor.call(name, function(error, v) {
+        				$("#" + candidates[name]).html(v.toString());
+      				});
+			} else {
+      				jag_contractInstance.count.call(name, function(error, v) {
+        				$("#" + candidates[name]).html(v.toString());
+      				});
+			}
     		} else {
     			Voting.deployed().then(function(contractInstance) {
      			console.log("toal_votes %s", name);
